64c5a2b2bd3ed75f324293b2037dcf6ad847e506
sablokasnajper <vid.skamen5@gmail.com>
2024-09-10 16:27:29 +0200

    follow counter

---
+++ followersCount/followers_count.js
@@ -0,0 +1,19 @@
+let count = 0;
+
+function increaseCount() {
+  count++; // by 1
+  displayCount(); // call the function to update the display
+  checkCountValue(); // call the function to check if the count meets the criteria
+}
+
+function displayCount() {
+  document.getElementById("countDisplay").innerHTML = count;
+}
+
+function checkCountValue() {
+  if (count == 10) {
+    alert("Congratulations on reaching 10 followers!");
+  } else if (count == 20) {
+    alert("Congratulations on reaching 20 followers! Keep it up!");
+  }
+}

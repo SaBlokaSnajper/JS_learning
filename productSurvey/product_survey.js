dd23adfdab1ce3c87d7a3b4a2048a132cf813013
sablokasnajper <vid.skamen5@gmail.com>
2024-09-10 17:33:42 +0200

    product survey

---
+++ productSurvey/product_survey.js
@@ -0,0 +1,28 @@
+const username = document.getElementById("name").value;
+const age = document.getElementById("age").value;
+const email = document.getElementById("email").value;
+const job = document.getElementById("job").value;
+const designation = document.getElementById("designation").value;
+const productType = document.getElementById("productType").value;
+const feedback = document.getElementById("feedbackText").value;
+const submitButton = document.getElementById("submitBtn").value;
+const experience = document.getElementById("experiencetext").value;
+function submitFeedback() {
+  submitButton.onclick = submitFeedback;
+  alert("Thank you for submitting your feedback");
+  document.getElementById("userName").innerHTML = username;
+  document.getElementById("userAge").innerHTML = age;
+  document.getElementById("userEmail").innerHTML = email;
+  document.getElementById("userJob").innerHTML = job;
+  document.getElementById("userDesignation").innerHTML = designation;
+  document.getElementById("userProductChoice").innerHTML = productType;
+  document.getElementById("userFeedback").innerHTML = feedback;
+  document.getElementById("userExperience").innerHTML = experience;
+  document.getElementById("userInfo").style.display = "block";
+}
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    submitFeedback();
+  }
+});

de91efe5d2552e88197921252cb711642a39674b
sablokasnajper <vid.skamen5@gmail.com>
2024-09-10 20:51:17 +0200

    added prices + all menus

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,21 @@
-const breakfastMenu = ["Pancakes", "Eggs Benedict", "Oatmeal", "Frittata"];
-const mainCourseMenu = ["Steak", "Pasta", "Burger", "Salmon"];
-const dessertMenu = ["Cake", "Ice Cream", "Pudding", "Fruit Salad"];
+const breakfastMenu = [
+  "Pancakes- $12",
+  "Eggs Benedict -$22.99",
+  "Oatmeal -$21.99",
+  "Frittata -$15",
+];
+const mainCourseMenu = [
+  "Steak- $86",
+  "Pasta- $45.50",
+  "Burger- $33.33",
+  "Salmon- $65.99",
+];
+const dessertMenu = [
+  "Cake- $15",
+  "Ice Cream- $4.99",
+  "Pudding- $6.50",
+  "Fruit Salad- $12.99",
+];
 
 const breakfastMenuItemsHTML = breakfastMenu
   .map((item, index) => `<p>item ${index + 1}: ${item}</p>`)
@@ -12,7 +27,7 @@ let mainCourseItem = "";
 mainCourseMenu.forEach((item, index) => {
   mainCourseItem += `<p>item ${index + 1}: ${item}</p>`;
 });
-document.getElementById("mainCourseMenuItems").innerHTML = mainCourseItem;
+document.getElementById("maincourseMenuItems").innerHTML = mainCourseItem;
 
 let dessertItem = "";
 for (let i = 0; i < dessertMenu.length; i++) {

34a3c71237ce2698fbaf4e0bc4150f9b09ea55ea
sablokasnajper <vid.skamen5@gmail.com>
2024-09-10 16:30:35 +0200

    reset followers

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -17,3 +17,9 @@ function checkCountValue() {
     alert("Congratulations on reaching 20 followers! Keep it up!");
   }
 }
+
+function resetCount() {
+  count = 0;
+  displayCount();
+  checkCountValue();
+}

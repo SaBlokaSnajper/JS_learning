f05a946194fbada91e9ce0dec510a5bcd2a913e3
sablokasnajper <vid.skamen5@gmail.com>
2024-09-11 18:16:44 +0200

    added specialization search

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,26 +1,41 @@
 const employees = [
-  { id: 1, name: "John Doe", age: 30, department: "IT", salary: 50000 },
-  { id: 2, name: "Alice Smith", age: 28, department: "HR", salary: 35000 },
-  { id: 3, name: "Bob Johnson", age: 35, department: "Finance", salary: 60000 },
+  {
+    id: 1,
+    name: "John Doe",
+    age: 30,
+    department: "IT",
+    salary: 50000,
+    specialization: "JavaScript",
+  },
+  {
+    id: 2,
+    name: "Alice Smith",
+    age: 28,
+    department: "HR",
+    salary: 35000,
+    specialization: "Python",
+  },
+  {
+    id: 3,
+    name: "Bob Johnson",
+    age: 35,
+    department: "Finance",
+    salary: 60000,
+    specialization: "Java",
+  },
 ];
 
 function totalEmployees() {
-  const employeeListDiv = document.getElementById("employeesDetails");
-  employeeListDiv.innerHTML = ""; // Clear any previous content
+  const employeeDetailsHTML = employees
+    .map(
+      (employee) =>
+        `<p>${employee.id}: ${employee.name} ${employee.age} - ${employee.department} - $${employee.salary}</p>`
+    )
+    .join("");
 
-  employees.forEach((employee) => {
-    const employeeDiv = document.createElement("div");
-    employeeDiv.innerHTML = `
-        <p><strong>ID:</strong> ${employee.id}</p>
-        <p><strong>Name:</strong> ${employee.name}</p>
-        <p><strong>Age:</strong> ${employee.age}</p>
-        <p><strong>Department:</strong> ${employee.department}</p>
-        <p><strong>Salary:</strong> $${employee.salary}</p>
-        <hr>
-      `;
-    employeeListDiv.appendChild(employeeDiv);
-  });
+  document.getElementById("employeesDetails").innerHTML = employeeDetailsHTML;
 }
+
 function calculateTotalSalaries() {
   const totalSalaries = employees.reduce(
     (acc, employee) => acc + employee.salary,
@@ -36,7 +51,7 @@ function displayHREmployees() {
   const hrEmployeesDisplay = hrEmployees
     .map(
       (employee, index) =>
-        `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`
+        `<p>${employee.id}: ${employee.name}: ${employee.age} - ${employee.department} - $${employee.salary}</p>`
     )
     .join("");
   document.getElementById("employeesDetails").innerHTML = hrEmployeesDisplay;
@@ -49,9 +64,22 @@ function findEmployeeById(employeeId) {
   if (foundEmployee) {
     document.getElementById(
       "employeesDetails"
-    ).innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.age} - ${foundEmployee.department} - ${foundEmployee.salary}: </p>`;
+    ).innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.age} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
   } else {
     document.getElementById("employeesDetails").innerHTML =
       "no employee found with this ID";
   }
+}
+
+function displayJavaScript() {
+  const jsEmployees = employees.filter(
+    (employee) => employee.specialization === "JavaScript"
+  );
+  const jsEmployeesDisplay = jsEmployees
+    .map(
+      (employee, index) =>
+        `<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.specialization}</p>`
+    )
+    .join("");
+  document.getElementById("employeesDetails").innerHTML = jsEmployeesDisplay;
 }
